dcda9a831c72132cffebd1def89ff7acb5627366
Luca Lambia <luca@lambia.it>
2026-01-27 18:39:39 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+	// const [dato, setDato] = useState(null);
+
+	return (
+		<div>
+			<h1>Hello React World</h1>
+		</div>
+	)
+}
+
+export default App
