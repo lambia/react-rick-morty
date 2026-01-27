221b4c1ca169ebd9714597d0bbbd9b61350d8493
Luca Lambia <luca@lambia.it>
2026-01-27 19:37:24 +0100

    chiamata per todolist

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+// const axios = require('axios'); //CommonJS (CJS)
+import axios from "axios"; //MJS
 
 function App() {
 
-	// const [dato, setDato] = useState(null);
+	const [todos, setTodos] = useState([]);
+
+	function getData() {
+		const apiUrl = "https://jsonplaceholder.typicode.com/todos";
+		axios.get(apiUrl).then((res) => {
+			console.log("Dati ricevuti da API", res.data);
+			setTodos(res.data)
+		})
+	}
+
+	function clearData() {
+		setTodos([]);
+	}
+
+	useEffect(getData, [])
 
 	return (
 		<div>
 			<h1>Hello React World</h1>
+			<button onClick={getData}>Recupera dati</button>
+			<button onClick={clearData}>Azzera dati</button>
+			<ul>
+				{todos.map((todo) => (
+					<li key={todo.id}>{todo.title}</li>
+				))}
+			</ul>
 		</div>
 	)
 }
